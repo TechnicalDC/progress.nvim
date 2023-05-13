d354370cd0581e37cdad7e250264cd0d6fcc6e85
Dilip Chauhan <chauhandilip305@gmail.com>
2023-05-13 23:02:41 +0530

    Added config

---
+++ lua/progress/default_config.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.options = {
+	dlc = "dlc"
+}
+
+M.get_config = function ()
+	return M.options
+end
+
+return M
---
+++ lua/progress/init.lua
@@ -0,0 +1,11 @@
+
+local config = require("progress.default_config")
+local bin = vim.fn.stdpath("data") .. "/lazy/progress.nvim/bin"
+
+local check_syntax = function ()
+end
+
+local run_program = function ()
+end
+
+
